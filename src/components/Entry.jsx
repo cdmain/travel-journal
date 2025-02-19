94f2f8f4e0131c38a0f4af7b7668c50dd7c1de84
cdhome <jordansewpershad@outlook.com>
2025-02-19 19:53:45 +0200

    Revert "Spread object as prop"
    This reverts commit e261a487ee1e3f57ac288d1ba81500ce36190c45.

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -2,21 +2,21 @@ export default function Entry(prop) {
     return (
         <article className="journal-entry">
             <div className="img-container">
-                <img className="country-img" src={prop.img.src} alt={prop.img.alt} />
+                <img className="country-img" src={prop.entry.img.src} alt={prop.entry.img.alt} />
             </div>   
 
             <div className="country-info">
                 <section className="location">
                     <img className="map-marker" src="src\assets\Fill 219.png" alt="map marker logo" />
-                    <span className="country-name">{prop.country}</span>
-                    <a className="google-maps-link" href={prop.googleMapsLink}>View on Google Maps</a>
+                    <span className="country-name">{prop.entry.country}</span>
+                    <a className="google-maps-link" href={prop.entry.googleMapsLink}>View on Google Maps</a>
                 </section>
 
-                <h2 className="city-name">{prop.title}</h2>
+                <h2 className="city-name">{prop.entry.title}</h2>
 
-                <p className="trip-date">{prop.dates}</p>
+                <p className="trip-date">{prop.entry.dates}</p>
                 
-                <p className="entry-text">{prop.text}</p>
+                <p className="entry-text">{prop.entry.text}</p>
             </div>
         </article>
     )
