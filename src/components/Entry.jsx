4f4fec7a883f79d004a175f784921280654a70bf
cdhome <jordansewpershad@outlook.com>
2025-02-21 12:39:12 +0200

    Entry update

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import mapMarkerIcon from "/src/assets/map-marker-icon.png";
 
 export default function Entry(prop) {
